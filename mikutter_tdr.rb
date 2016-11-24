0d79f96189db0e130d54c1416a023c0be8f1d5bf
yuzumone <yuzumone@outlook.com>
2016-11-24 14:42:01 +0900

    ふぁーすとこみっと

---
+++ mikutter_tdr.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:mikutter_datasource_tdr) do
+  require 'kconv'
+  
+  filter_extract_datasources { |datasources|
+    begin
+      datasources[:mikutter_tdl_greeting] = "TDL Greeting"
+    rescue => e
+      puts e
+      puts e.backtrace
+    end
+
+    [datasources]
+  }
+
+  today = Date.today.strftime("%Y%m%d")
+  url = 'http://info.tokyodisneyresort.jp/s/daily_schedule/show/tdl_' + today + '.html'
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset
+    f.read
+  end
+
+  msgs = []
+  doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
+  greetings = doc.css('ul#greeting')
+  greeting_list = greetings.css('li')
+  greeting_list.each do |g|
+    name = g.css('h3').text.gsub(/(\s)/,"")
+    times = g.css('p.time').text
+    text = name + "\n" + times
+    if (/\d+:\d+/ === times)
+      time = times.match(/\d+:\d+/)[0]
+      msg = Message.new(:message => text , :system => true)
+      msg[:modified] = Time.parse(time)
+      user = User.new(:id => 3939, :idname => "TDL Greeting")
+      user[:profile_image_url] = File.join(File.dirname(__FILE__), "tdl.png")
+      msg[:user] = user
+      msgs.push(msg)
+    end
+  end
+  Plugin.call(:extract_receive_message, :mikutter_tdl_greeting, msgs)
+end
