6e0a92608c3816a90699bf935d74a8a8df5170f6
yuzumone <yuzumone@outlook.com>
2017-10-26 11:27:25 +0900

    前のやつを削除

--- api/model.rb
+++ api/model.rb
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 
-require_relative 'model/park'
-require_relative 'model/attraction'
-require_relative 'model/greeting'
-require_relative 'model/site'
-require_relative 'model/weather'
-require_relative 'model/restaurant'
-require_relative 'model/rehab'
-require_relative 'model/character'
