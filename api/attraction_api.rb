8eaa530a5b620c1ef0af549e0bfd5e5c382c3988
yuzumone <yuzumone@outlook.com>
2018-05-19 10:47:05 +0900

    アトラクションデータを取得できるように修正
    close #21

--- api/attraction_api.rb
+++ api/attraction_api.rb
@@ -16,8 +16,6 @@ module Plugin::TDR
       reserver
     end
 
-    private
-    # アトラクションのアップデートを5分ごとに繰り返し実行
     def reserver
       Reserver.new(300) {
         fetch_tdl
@@ -26,37 +24,17 @@ module Plugin::TDR
       }
     end
 
-    # TDLのアトラクションの情報を取得
+    private
     def fetch_tdl
       Thread.new {
-        url = 'http://info.tokyodisneyresort.jp/rt/s/gps/tdl_index.html' +
-            '?nextUrl=http://info.tokyodisneyresort.jp/rt/s/realtime/tdl_attraction.html' +
-            '&lat=35.6329527&lng=139.8840281'
-        Plugin::TDR::API.get_content_with_redirection url
-      }.next { |response|
-        doc = Nokogiri::HTML.parse(response, nil, 'utf-8')
-        doc.css('ul#atrc.schedule').css('li')
-      }.next { |doc|
+        url = 'https://www.tokyodisneyresort.jp/_/realtime/tdl_attraction.json'
+        res = Plugin::TDR::API.fetch url
         [Plugin::TDR::User.new(
-            name: '東京ディズニーランド アトラクション',
-            profile_image_url: File.join(File.dirname(__FILE__), '../tdl.png')
-        ), doc]
-      }.next { |park, doc|
-        msgs = []
-        if doc.empty?
-          msg = Plugin::TDR::Information.new(
-              name: '閉園',
-              text: 'ただいま東京ディズニーランドは、閉園しております。',
-              link: 'http://info.tokyodisneyresort.jp/s/calendar/tdl/',
-              created: Time.now,
-              modified: Time.now,
-              user: park)
-          msgs.push(msg)
-        else
-          msgs = create_message park, doc
-        end
-        msgs
-      }.next { |msgs|
+          name: 'ディズニーランド アトラクション',
+          profile_image_url: File.join(File.dirname(__FILE__), '../tdl.png')
+        ), res]
+      }.next { |park, res|
+        msgs = create_message park, res
         Plugin.call :destroyed, @saved_tdl_attractions
         Plugin.call :appear, msgs
         Plugin.call :extract_receive_message, :mikutter_tdl_attraction, msgs
@@ -64,38 +42,16 @@ module Plugin::TDR
       }.trap { |e| error e }
     end
 
-    # TDSのアトラクションの情報を取得
     def fetch_tds
       Thread.new {
-        url = 'http://info.tokyodisneyresort.jp/rt/s/gps/tds_index.html' +
-            '?nextUrl=http://info.tokyodisneyresort.jp/rt/s/realtime/tds_attraction.html' +
-            '&lat=35.6329527&lng=139.8840281'
-        Plugin::TDR::API.get_content_with_redirection url
-      }.next { |response|
-        doc = Nokogiri::HTML.parse(response, nil, 'utf-8')
-        doc.css('ul#atrc.schedule').css('li')
-      }.next { |doc|
-        [Plugin::TDR::User.new(
-            name: '東京ディズニーシー アトラクション',
-            profile_image_url: File.join(File.dirname(__FILE__), '../tds.png')
-        ), doc]
-      }.next { |park, doc|
-        msgs = []
-        if doc.empty?
-          msg = Plugin::TDR::Information.new(
-              name: '閉園',
-              text: 'ただいま東京ディズニーシーは、閉園しております。',
-              link: 'http://info.tokyodisneyresort.jp/s/calendar/tds/',
-              created: Time.now,
-              modified: Time.now,
-              user: park
-          )
-          msgs.push(msg)
-        else
-          msgs = create_message park, doc
-        end
-        msgs
-      }.next { |msgs|
+        url = 'https://www.tokyodisneyresort.jp/_/realtime/tds_attraction.json'
+        res = Plugin::TDR::API.fetch url
+        [ Plugin::TDR::User.new(
+          name: 'ディズニーシー アトラクション',
+          profile_image_url: File.join(File.dirname(__FILE__), '../tds.png')
+        ), res]
+      }.next { |park, res|
+        msgs = create_message park, res
         Plugin.call :destroyed, @saved_tds_attractions
         Plugin.call :appear, msgs
         Plugin.call :extract_receive_message, :mikutter_tds_attraction, msgs
@@ -103,28 +59,28 @@ module Plugin::TDR
       }.trap { |e| error e }
     end
 
-    def create_message(park, doc)
-      doc.map.with_index do |attraction, i|
-        name = attraction.css('h3').text.gsub(/(\s)/, '')
-        wait_time = attraction.css('p.waitTime').text.gsub(/(\s)/, '')
-        run_time = attraction.css('p.run').text.gsub(/(\s)/, '')
-        fp_time = attraction.css('p.fp').text.gsub(/(\s)/, '')
+    def create_message park, data
+      data.map.with_index { | attraction, i |
+        name = attraction['FacilityName']
+        status = attraction['OperatingStatus']
+        time = attraction['StandbyTime'] || nil
+        url = attraction['FacilityURLSP']
+        fp = attraction['FsStatus']
+        update = attraction['UpdateTime']
         text = name
-        text = text + "\n" + run_time unless run_time.empty?
-        text = text + "\n待ち時間: " + wait_time unless wait_time.empty?
-        text = text + "\nFP: " + fp_time unless fp_time.empty?
+        text += "\n" + status unless status.nil?
+        text += "\n" + time + ' 分' unless time.nil?
+        text += "\n" + 'FP: ' + fp unless fp.nil?
+        text += "\n" + '更新時間: ' + update
         msg = Plugin::TDR::Information.new(
-            name: name,
-            text: text,
-            created: Time.now,
-            modified: Time.now - i,
-            user: park
+          name: name,
+          text: text,
+          created: Time.now - i,
+          modified: Time.now,
+          user: park,
+          link: url
         )
-        unless attraction.css('a').empty?
-          msg.link = attraction.css('a').attribute('href')
-        end
-        msg
-      end
+      }
     end
   end
 end
