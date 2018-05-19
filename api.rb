62874a5bc317ee3f214d14ebffe1a8b669b163c4
yuzumone <yuzumone@outlook.com>
2018-05-19 10:47:05 +0900

    アトラクションデータを取得できるように修正

--- api.rb
+++ api.rb
@@ -10,15 +10,24 @@ require_relative 'api/weather_api'
 module Plugin::TDR
   class API
     class << self
-      # リダイレクト先のURLを取得
-      # http://opentechnica.blogspot.jp/2012/10/rubyurlurl.html
-      def get_content_with_redirection(url)
+      def fetch url
         client = HTTPClient.new
-        def client.allow_all_redirection_callback(uri, res)
-          urify(res.header['location'][0])
-        end
-        client.redirect_uri_callback = client.method(:allow_all_redirection_callback)
-        client.get_content url
+        header = {
+            'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding' => 'gzip, deflate, br',
+            'Accept-Language' => 'ja',
+            'Upgrade-Insecure-Requests' => '1',
+            'Connection' => 'keep-alive'
+        }
+        cookie = WebAgent::Cookie.new
+        cookie.name = 'tdrloc'
+        cookie.value = UserConfig[:mikutter_tdr_cookie_value]
+        cookie.url = Addressable::URI.parse('https://www.tokyodisneyresort.jp/view_interface.php?blockId=94199&pageBlockId=13476&nextUrl=tdlattraction')
+        client.cookie_manager.add cookie
+        res = client.get(url, :header => header)
+        body = Zlib::GzipReader.new(StringIO.new(res.body.to_s)).read
+        JSON.parse body
       end
     end
   end
