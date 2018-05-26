30ffaf07b285320607b4a97e6a770309f1259c15
yuzumone <yuzumone@outlook.com>
2018-05-26 15:27:21 +0900

    リハブ直した
    close #25

--- api/rehab_api.rb
+++ api/rehab_api.rb
@@ -10,16 +10,26 @@ module Plugin::TDR
     end
 
     private
-    # TDLのリハブ情報を取得
+    def fetch url
+      client = HTTPClient.new
+      header = {
+          'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+          'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Encoding' => 'gzip, deflate, br',
+          'Accept-Language' => 'ja',
+          'Upgrade-Insecure-Requests' => '1',
+          'Connection' => 'keep-alive'
+      }
+      res = client.get(url, :header => header)
+      body = Zlib::GzipReader.new(StringIO.new(res.body.to_s)).read
+      doc = Nokogiri::HTML.parse(body, nil, 'utf-8')
+      doc.xpath('//*[@class="linkList6"]/ul/li')
+    end
+
     def fetch_tdl
       Thread.new {
-        client = HTTPClient.new
-        url = 'http://info.tokyodisneyresort.jp/schedule/stop/stop_list.html'
-        client.get(url)
-      }.next { |response|
-        charset = response.body_encoding.name
-        doc = Nokogiri::HTML.parse(response.content, nil, charset)
-        doc.xpath('//*[@id="main"]/div/div/section[1]/dl').css('li')
+        url = 'http://www.tokyodisneyresort.jp/tdl/monthly/stop.html'
+        fetch url
       }.next { |doc|
         [Plugin::TDR::User.new(
             name: '東京ディズニーランド リハブ情報',
@@ -33,16 +43,10 @@ module Plugin::TDR
       }.trap { |e| error e }
     end
 
-    # TDSのリハブ情報を取得
     def fetch_tds
       Thread.new {
-        client = HTTPClient.new
-        url = 'http://info.tokyodisneyresort.jp/schedule/stop/stop_list.html'
-        client.get(url)
-      }.next { |response|
-        charset = response.body_encoding.name
-        doc = Nokogiri::HTML.parse(response.content, nil, charset)
-        doc.xpath('//*[@id="main"]/div/div/section[2]/dl').css('li')
+        url = 'http://www.tokyodisneyresort.jp/tds/monthly/stop.html'
+        fetch url
       }.next { |doc|
         [Plugin::TDR::User.new(
             name: '東京ディズニーシー リハブ情報',
@@ -57,17 +61,16 @@ module Plugin::TDR
     end
 
     def create_message(park, doc)
-      doc.map.with_index do |rehab, i|
-        name = rehab.css('a').inner_text != '' ? rehab.css('a').inner_text : rehab.css('p').text
-        date = rehab.css('span').text
+      doc.map.with_index { |rehab, i|
+        text = rehab.css('p').map { |i| i.text.gsub(/(\s)/, '') }.join("\n")
         Plugin::TDR::Information.new(
-            name: name,
-            text: "#{name}\n#{date}",
-            created: Time.now,
-            modified: Time.now - i,
+            name: 'リハブ',
+            text: text,
+            created: Time.now - i,
+            modified: Time.now,
             user: park
         )
-      end
+        }.select {|item| item.text != "休止を予定しているものはありません。"}
     end
   end
 end
